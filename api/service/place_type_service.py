97299b6a1b219a9140a427ecc95ab0a7187a85aa
AlkseyPV <leha041304@gmail.com>
2025-08-17 19:19:48 +0300

    fix tab

--- api/service/place_type_service.py
+++ api/service/place_type_service.py
@@ -8,6 +8,6 @@ class PlaceTypeService:
     async def create_place_type(self, db:AsyncSession, place_type:CreatePlaceType):
         return await PlaceTypeRepository.create_place_type(db,place_type)
     
-async def get_all_place_types(self, db: AsyncSession) -> list[PlaceTypeResponse]:
-    place_types = await PlaceTypeRepository.get_all_place_types(db)
-    return [PlaceTypeResponse(id=pt.id, title=pt.title) for pt in place_types]
+    async def get_all_place_types(self, db: AsyncSession) -> list[PlaceTypeResponse]:
+        place_types = await PlaceTypeRepository.get_all_place_types(db)
+        return [PlaceTypeResponse(id=pt.id, title=pt.title) for pt in place_types]
